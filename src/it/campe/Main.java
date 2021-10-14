4948a8776394e7f629ae1b82a6f92b65cd95f30a
Ksenx73 <hello@itcampe.com>
2021-10-14 16:54:49 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,14 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+
+        String digits = ""; //напишите тут ваш код
+
+        System.out.println(digits);
+    }
+}
